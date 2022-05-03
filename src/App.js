488b280d03745e0e8d23e624e41373afe8a88d41
Alexander Brenner <a.reinhardbrenner@gmail.com>
2022-05-03 19:44:45 +0200

    Remove warnings

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from 'react';
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Base from './components/base/base.js'
 import Login from './components/login/login.js'
 
